8c11c092442c4523ba012f62d6c9375dd2044bf9
Gregor de Cillia <de.cillia.gregor@gmail.com>
2025-06-06 09:45:59 +0200

    prettier

--- src/calculator/script.jsx
+++ src/calculator/script.jsx
@@ -3,66 +3,76 @@ import "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js";
 
 const buttons = [
     { symbol: "AC", width: 2, id: "clear" },
-    { symbol: "x",  width: 1, id: "multiply" },
-    { symbol: "/",  width: 1, id: "divide" },
-    { symbol: "7",  width: 1, id: "seven" },
-    { symbol: "8",  width: 1, id: "eight" },
-    { symbol: "9",  width: 1, id: "nine" },
-    { symbol: "-",  width: 1, id: "subtract" },
-    { symbol: "4",  width: 1, id: "four" },
-    { symbol: "5",  width: 1, id: "five" },
-    { symbol: "6",  width: 1, id: "six" },
-    { symbol: "+",  width: 1, id: "add" },
-    { symbol: "1",  width: 1, id: "one" },
-    { symbol: "2",  width: 1, id: "two" },
-    { symbol: "3",  width: 1, id: "three" },
-    { symbol: ".",  width: 1, id: "decimal" },
-    { symbol: "0",  width: 2, id: "zero"  },
-    { symbol: "=",  width: 2, id: "equals"}
-]
+    { symbol: "x", width: 1, id: "multiply" },
+    { symbol: "/", width: 1, id: "divide" },
+    { symbol: "7", width: 1, id: "seven" },
+    { symbol: "8", width: 1, id: "eight" },
+    { symbol: "9", width: 1, id: "nine" },
+    { symbol: "-", width: 1, id: "subtract" },
+    { symbol: "4", width: 1, id: "four" },
+    { symbol: "5", width: 1, id: "five" },
+    { symbol: "6", width: 1, id: "six" },
+    { symbol: "+", width: 1, id: "add" },
+    { symbol: "1", width: 1, id: "one" },
+    { symbol: "2", width: 1, id: "two" },
+    { symbol: "3", width: 1, id: "three" },
+    { symbol: ".", width: 1, id: "decimal" },
+    { symbol: "0", width: 2, id: "zero" },
+    { symbol: "=", width: 2, id: "equals" },
+];
 
-const Button = ({set, num, width, id}) => {
-  num = num.toString()
-  const style = {
-    height: "3rem", background: "grey", cursor: "pointer",
-    color: "white", display: "flex", "alignItems": "center",
-    "justifyContent": "center", fontWeigt:800, fontSize: "2rem"
-  }
-  if (width === 2) {
-    style.gridColumn = "span 2"
-  }
-  const handleClick = _ => set(num)
-  return <div className="key" style={style} onClick={handleClick} id={id}>{num}</div>
-}
+const Button = ({ set, num, width, id }) => {
+    num = num.toString();
+    const style = {
+        height: "3rem",
+        background: "grey",
+        cursor: "pointer",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontWeigt: 800,
+        fontSize: "2rem",
+    };
+    if (width === 2) {
+        style.gridColumn = "span 2";
+    }
+    const handleClick = (_) => set(num);
+    return (
+        <div className="key" style={style} onClick={handleClick} id={id}>
+            {num}
+        </div>
+    );
+};
 
-const Grid = ({children}) => {
-  const style = {display: "grid", gap: ".2rem", 
-                 gridTemplateColumns: "repeat(4, 1fr)"}
-  return <div style={style}>{children}</div>
-}
+const Grid = ({ children }) => {
+    const style = { display: "grid", gap: ".2rem", gridTemplateColumns: "repeat(4, 1fr)" };
+    return <div style={style}>{children}</div>;
+};
 
 const App = () => {
-  const [text, setText] = React.useState('0');
-  const handleInput = (input) => {
-    console.log(text)
-    if (input === "=")
-      return setText(text => eval(text.replaceAll("x", "*")))
-    if (input === "AC")
-      setText(_ => "0")
-    else
-      setText(text => text === "0" ? input : text + input)
-  }
-  return <div id="calculator" style={{marginTop: "4rem"}}>
-    <div id="display" style={{textAlign: "right"}}>{text}</div>
-    <Grid>
-      {
-        buttons.map(el => {
-          return <Button set={handleInput} num={el.symbol} width={el.width}
-            id={el.id}/>
-        })
-      }
-    </Grid>
-  </div>
-}
+    const [text, setText] = React.useState("0");
+    const handleInput = (input) => {
+        console.log(text);
+        if (input === "=") return setText((text) => eval(text.replaceAll("x", "*")));
+        if (input === "AC") setText((_) => "0");
+        else
+            setText((text) => {
+                return text === "0" ? input : text + input;
+            });
+    };
+    return (
+        <div id="calculator" style={{ marginTop: "4rem" }}>
+            <div id="display" style={{ textAlign: "right" }}>
+                {text}
+            </div>
+            <Grid>
+                {buttons.map((el) => {
+                    return <Button set={handleInput} num={el.symbol} width={el.width} id={el.id} />;
+                })}
+            </Grid>
+        </div>
+    );
+};
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
