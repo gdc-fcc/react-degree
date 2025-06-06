f62af362b5863184eb8203737574e4f3516716c6
GregorDeCillia <GregorDeCillia@users.noreply.github.com>
2025-06-06 07:58:31 +0000

    deploy: 8c11c092442c4523ba012f62d6c9375dd2044bf9

---
+++ calculator/script.js
@@ -0,0 +1,142 @@
+import "https://unpkg.com/react@16/umd/react.production.min.js";
+import "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js";
+const buttons = [{
+  symbol: "AC",
+  width: 2,
+  id: "clear"
+}, {
+  symbol: "x",
+  width: 1,
+  id: "multiply"
+}, {
+  symbol: "/",
+  width: 1,
+  id: "divide"
+}, {
+  symbol: "7",
+  width: 1,
+  id: "seven"
+}, {
+  symbol: "8",
+  width: 1,
+  id: "eight"
+}, {
+  symbol: "9",
+  width: 1,
+  id: "nine"
+}, {
+  symbol: "-",
+  width: 1,
+  id: "subtract"
+}, {
+  symbol: "4",
+  width: 1,
+  id: "four"
+}, {
+  symbol: "5",
+  width: 1,
+  id: "five"
+}, {
+  symbol: "6",
+  width: 1,
+  id: "six"
+}, {
+  symbol: "+",
+  width: 1,
+  id: "add"
+}, {
+  symbol: "1",
+  width: 1,
+  id: "one"
+}, {
+  symbol: "2",
+  width: 1,
+  id: "two"
+}, {
+  symbol: "3",
+  width: 1,
+  id: "three"
+}, {
+  symbol: ".",
+  width: 1,
+  id: "decimal"
+}, {
+  symbol: "0",
+  width: 2,
+  id: "zero"
+}, {
+  symbol: "=",
+  width: 2,
+  id: "equals"
+}];
+const Button = ({
+  set,
+  num,
+  width,
+  id
+}) => {
+  num = num.toString();
+  const style = {
+    height: "3rem",
+    background: "grey",
+    cursor: "pointer",
+    color: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeigt: 800,
+    fontSize: "2rem"
+  };
+  if (width === 2) {
+    style.gridColumn = "span 2";
+  }
+  const handleClick = _ => set(num);
+  return /*#__PURE__*/React.createElement("div", {
+    className: "key",
+    style: style,
+    onClick: handleClick,
+    id: id
+  }, num);
+};
+const Grid = ({
+  children
+}) => {
+  const style = {
+    display: "grid",
+    gap: ".2rem",
+    gridTemplateColumns: "repeat(4, 1fr)"
+  };
+  return /*#__PURE__*/React.createElement("div", {
+    style: style
+  }, children);
+};
+const App = () => {
+  const [text, setText] = React.useState("0");
+  const handleInput = input => {
+    console.log(text);
+    if (input === "=") return setText(text => eval(text.replaceAll("x", "*")));
+    if (input === "AC") setText(_ => "0");else setText(text => {
+      return text === "0" ? input : text + input;
+    });
+  };
+  return /*#__PURE__*/React.createElement("div", {
+    id: "calculator",
+    style: {
+      marginTop: "4rem"
+    }
+  }, /*#__PURE__*/React.createElement("div", {
+    id: "display",
+    style: {
+      textAlign: "right"
+    }
+  }, text), /*#__PURE__*/React.createElement(Grid, null, buttons.map(el => {
+    return /*#__PURE__*/React.createElement(Button, {
+      set: handleInput,
+      num: el.symbol,
+      width: el.width,
+      id: el.id
+    });
+  })));
+};
+ReactDOM.render(/*#__PURE__*/React.createElement(App, null), document.getElementById("app"));
+//# sourceMappingURL=script.js.map
